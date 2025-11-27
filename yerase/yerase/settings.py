82dcb9b4c9d8c2b4f9d84eb7bc6b4057f80b8127
Natnael Abebe <150276515+NatnaelAbebelab@users.noreply.github.com>
2025-11-27 14:06:22 +0300

    Update settings.py

--- yerase/yerase/settings.py
+++ yerase/yerase/settings.py
@@ -31,13 +31,6 @@ INSTALLED_APPS = [
     'drf_spectacular'
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'natnaelabebelab@gmail.com'
-EMAIL_HOST_PASSWORD = 'fvdygngngnqmoter'  # Use environment variables for security
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
